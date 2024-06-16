5d012d79c2a543ea1be449decd48e098ed04c152
Kevin <kevinmantovani23@gmail.com>
2024-06-15 23:27:23 -0300

    telainicial

---
+++ Blibyos/src/boundary/TelaInicial.java
@@ -0,0 +1,16 @@
+package boundary;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TelaInicial extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
